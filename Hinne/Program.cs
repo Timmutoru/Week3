5fd0e5a9063ab70e44bdeb4a919b2a4f14cf2fc0
Timmutoru <124074292+Timmutoru@users.noreply.github.com>
2023-05-09 18:40:00 +0300

    Add files via upload

---
+++ Hinne/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Please enter your grade");
+Console.ReadLine());
+
+if (testScore >= 5)
+{
+    Console.WriteLine("A");
+}
+else if (testScore >= 4)
+
+{
+    Console.WriteLine("B");
+}
+else if (testScore >= 3)
+
+{
+    Console.WriteLine("C");
+}
+else if (testScore >= 2)
+
+{
+    Console.WriteLine("D");
+}
+else if (testScore >= 1)
+
+{
+    Console.WriteLine("E");
+}
+else
+{
+    Console.WriteLine("F");
+} 
